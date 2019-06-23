32429a93434531c7c459082df1e701922ca6c7e6
jose munoz <jose.venegas1411@gmail.com>
2019-06-23 11:36:07 -0400

    first commit

---
+++ app/src/main/java/com/josemunoz/kengen/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.josemunoz.kengen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //el primero kotlin
+    }
+}
