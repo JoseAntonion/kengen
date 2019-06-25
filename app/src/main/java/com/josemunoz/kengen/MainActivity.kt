57ef361bbc8c7f299bcfd7ca2bb6cbea1dcd14d8
jose munoz <jose.venegas1411@gmail.com>
2019-06-25 00:55:19 -0400

    testing with toast

--- app/src/main/java/com/josemunoz/kengen/MainActivity.kt
+++ app/src/main/java/com/josemunoz/kengen/MainActivity.kt
@@ -2,6 +2,9 @@ package com.josemunoz.kengen
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //el primero kotlin
+        var firstTextView = findViewById<TextView>(R.id.firstText)
+        var mensaje = "holanda que talca"
+        firstTextView.setText(mensaje)
+        Toast.makeText(this,mensaje,LENGTH_SHORT).show()
     }
 }
